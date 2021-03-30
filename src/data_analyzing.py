042f2fe643a33136bce4dc485b294ea348b7ba75
MrRachidK <rachid.karbiche.pro@gmail.com>
2021-03-30 15:23:36 +0200

    Relative path for the database

--- src/data_analyzing.py
+++ src/data_analyzing.py
@@ -4,15 +4,12 @@ import pandas as pd
 ### 1 - Import and informations provided by the database ###
 
 # Import of the database
-taxi_analyze = pd.read_csv('/home/apprenant/Documents/Brief-7-Taxi-NYC/data/01_raw/train.csv').sample(n=10000)
+taxi_analyze = pd.read_csv('data/01_raw/train.csv').sample(n=10000)
 
 # Some infos about the database (columns and shape of database)
 print('Nous étudions une base de données de {} lignes et de {} colonnes.'.format(taxi_analyze.shape[0], taxi_analyze.shape[1]))
 print('---------------------------')
 print('Voici la liste des colonnes de notre base de données : {}'.format(taxi_analyze.columns.tolist()))
-print('---------------------------')
-# Display of all the types of each variable
-print(taxi_analyze.dtypes)
 print('---------------------------')
 
 # Display of the five first entries of the database
@@ -33,18 +30,36 @@ for i in taxi_analyze.columns.tolist():
     print('Dans la colonne {}, nous avons {} % de valeurs manquantes'.format(i, part_missing_values))
 print('---------------------------')
 
-# Conversion of the pickup_datetime and dropoff_datetime columns into datetime data type
-taxi_analyze['pickup_datetime']=pd.to_datetime(taxi_analyze['pickup_datetime'])
-taxi_analyze['dropoff_datetime']=pd.to_datetime(taxi_analyze['dropoff_datetime'])
-
 ### 3 - Let's check the duplicated entries ###
 
 print(taxi_analyze.duplicated().value_counts()) # Output : 'False 10000', so there are no perfect duplicated entries
 print(taxi_analyze.id.duplicated().value_counts()) # Output : 'False 10000', so there are no duplicated entries
-
+print('---------------------------')
 ### 4 - Let's check the missing values ###
 
-# We checked previously that there are no missing values in each column, so we can move on the next step
+# We checked previously that there are no missing values in each column, so we can move on to the next step
 
-### 5 - 
+### 5 - Let's analyze the types of our datas
 
+# Display of all the types of each variable
+print(taxi_analyze.dtypes)
+print('---------------------------')
+
+# Conversion of the pickup_datetime and dropoff_datetime columns into datetime data type
+taxi_analyze['pickup_datetime']=pd.to_datetime(taxi_analyze['pickup_datetime'])
+taxi_analyze['dropoff_datetime']=pd.to_datetime(taxi_analyze['dropoff_datetime'])
+
+### 6 - Let's analyze the relevance of our database ###
+
+# Let's check the unique values of some columns
+
+print(sorted(taxi_analyze['vendor_id'].unique()))
+print('---------------------------')
+print(sorted(taxi_analyze['passenger_count'].unique()))
+print('---------------------------')
+print(sorted(taxi_analyze['store_and_fwd_flag'].unique()))
+
+
+
+
+# sampled_taxi_trips = taxi_analyze.to_csv('/home/apprenant/Documents/Brief-7-Taxi-NYC/data/02_intermediate/sampled_train.csv')
